49200992df1270628e651c63857dbb9c1c53605d
Kishan <kishanverma371997@gmail.com>
2024-07-13 15:13:15 +0530

    add gitkeeep in public folder

--- portfolio_backend/src/middlewares/multer.middleware.js
+++ portfolio_backend/src/middlewares/multer.middleware.js
@@ -1,8 +1,25 @@
 import multer from "multer";
+import fs from 'fs';
+import path from "path";
+import { fileURLToPath } from 'url';
+
+// Convert the URL to a file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const tempDir = path.join(__dirname, '../../public/temp')
+
+const ensureDirectoryExistance = (dir) => {
+    if(!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, {recursive:true});
+    }
+};
+
+ensureDirectoryExistance(tempDir)
 
 const storage = multer.diskStorage({
     destination: function (req,file, cb){
-        cb(null, "../../public/temp")
+        cb(null, tempDir)
     },
     filename: function(req, file, cb){
         cb(null, file.originalname)
