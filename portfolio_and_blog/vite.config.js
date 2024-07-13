034fd9202a73675c8f863feec34ab34d6af152b8
Kishan <kishanverma371997@gmail.com>
2024-07-13 17:07:46 +0530

    add vercel config , env and change in gitigonre

--- portfolio_and_blog/vite.config.js
+++ portfolio_and_blog/vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig ,loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api':'https://my-portfolio-fullstack-backend.onrender.com',
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` (development, production, etc.)
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    server: {
+      proxy: {
+        '/api': env.VITE_BASE_URL,
+      },
     },
-  },
-  plugins: [react()],
-})
+    plugins: [react()],
+  };
+});
