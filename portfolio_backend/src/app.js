26d38d8e5d8aee5ede8864545397fc7b55c7330d
Kishan <kishanverma371997@gmail.com>
2024-07-16 12:40:27 +0530

    update cros

--- portfolio_backend/src/app.js
+++ portfolio_backend/src/app.js
@@ -4,11 +4,12 @@ import cookieParser from 'cookie-parser'
 
 const app = express()
 
-app.use(cors({
-    origin:process.env.CROS_ORIGIN,
-    credentials:true
-}))
+// app.use(cors({
+//     origin:process.env.CROS_ORIGIN,
+//     credentials:true
+// }))
 
+app.use(cors())
 app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({extended:true, limit:"50mb"}))
 app.use(express.static("public"))
