d7d2d4984847ce013b202d1e6d3b03eaf4dc5020
Kishan <kishanverma371997@gmail.com>
2024-07-21 12:35:31 +0530

    test the config

--- portfolio_and_blog/vite.config.js
+++ portfolio_and_blog/vite.config.js
@@ -6,15 +6,15 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    // server: {
-    //   proxy: {
-    //     '/api/v1': {
-    //       target: 'http://127.0.0.1:8000',
-    //       changeOrigin: true,
-    //       rewrite: (path) => path.replace(/^\/api/, '/api')
-    //     },
-    //   },
-    // },
+    server: {
+      proxy: {
+        '/api/v1': {
+          target: 'https://my-portfolio-fullstack-backend.onrender.com',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '/api')
+        },
+      },
+    },
     plugins: [react()],
   };
 });
