381b7825507e7ef159e919cc5259f0731dd8da77
Krishan Verma <109855558+Kishan3799@users.noreply.github.com>
2024-09-11 19:48:27 +0530

    Update name Home.jsx

--- portfolio_and_blog/src/containers/home/Home.jsx
+++ portfolio_and_blog/src/containers/home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <section className="app__hero-section container">
       <div className="hero-content">
         <h2>Hi, I'm</h2>
-        <h1>Kishan Verma</h1>
+        <h1>Krishan Verma</h1>
         <h2>Android Developer | Web Developer </h2>
         <p>Passionate about creating innovative solutions, designing beautiful UIs, and developing user-friendly applications.</p>
         {/* <button className="cta-button"><FaFileDownload/> Resume</button> */}
